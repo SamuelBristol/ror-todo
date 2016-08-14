93702331a6f6da5c3c47b0206bf0c09b646d3224
Sam Bristol <samuel.d.bristol@gmail.com>
2016-08-14 21:36:14 +0000

    Scaffold Todo, add ajax crud calls

--- db/migrate/20160813201604_create_todos.rb
+++ db/migrate/20160813201604_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[5.0]
   def change
     create_table :todos do |t|
       t.string :title
-      t.text :text
+      t.boolean :complete
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811174212) do
+ActiveRecord::Schema.define(version: 20160813201604) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
-    t.text     "text"
+    t.boolean  "complete"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
